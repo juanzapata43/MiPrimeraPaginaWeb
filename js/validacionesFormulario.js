1671f42482a541c8143efccd44238660ab638231
juanzapata43 <149023876+juanzapata43@users.noreply.github.com>
2023-12-03 19:40:47 -0500

    Update validacionesFormulario.js

--- js/validacionesFormulario.js
+++ js/validacionesFormulario.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Función para mostrar u ocultar el mensaje de campo requerido
   function toggleCampoRequerido(elemento) {
     var mensajeError = elemento.nextElementSibling;
     if (mensajeError && mensajeError.classList.contains('campo-requerido')) {
@@ -11,22 +10,18 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   }
-
-  // Añadir evento input a cada campo para controlar la visualización del mensaje de error
   var campos = document.querySelectorAll('#nombreProducto, #precioProducto, #cantidadProducto, #categoriaProducto');
   campos.forEach(function(campo) {
-    // Inicializar en el arranque en caso de que los campos estén pre-rellenados
+   
     toggleCampoRequerido(campo);
     campo.addEventListener('input', function() {
       toggleCampoRequerido(campo);
     });
   });
 
-  // Manejador del evento submit del formulario
   document.querySelector('form').addEventListener('submit', function(event) {
     var formValido = true;
 
-    // Validaciones de cada campo excepto el campo de la imagen
     campos.forEach(function(campo) {
       if (campo.value.trim() === '') {
         toggleCampoRequerido(campo);
@@ -34,13 +29,12 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
 
-    // Si el formulario no es válido, prevenir el envío
     if (!formValido) {
       event.preventDefault();
     } else {
-      // Mostrar alerta de producto creado y recargar la página
+      
       alert('Producto creado');
-      location.reload(); // Esto recargará la página después de aceptar la alerta
+      location.reload(); 
     }
   });
 });
